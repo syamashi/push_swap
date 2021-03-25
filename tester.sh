bbb8846f152ea4393d77cbf980b58ee0e917e96d
syamashi <syamashi@student.42tokyo.jp>
2021-03-25 17:30:16 +0900

    tester

--- tester.sh
+++ tester.sh
@@ -13,8 +13,8 @@ while [ "$INDEX" -lt "$COUNT" ]
 do
 
 ARG=`jot -r -s " " 100 -2147483648 2147483647`
-./push_swap_src/push_swap $ARG | ./checker_src/checker $ARG | xargs echo -n
-TMP=`./push_swap_src/push_swap $ARG | wc -l`
+./push_swap $ARG | ./checker $ARG | xargs echo -n
+TMP=`./push_swap $ARG | wc -l`
 if [ $TMP -gt $MAX100 ] ; then
     MAX100=$TMP
     echo $ARG > $FILE1
@@ -22,8 +22,8 @@ fi
 echo -n " $TMP  "
 
 ARG=`jot -r -s " " 500 -2147483648 2147483647`
-./push_swap_src/push_swap $ARG | ./checker_src/checker $ARG | xargs echo -n
-TMP=`./push_swap_src/push_swap $ARG | wc -l`
+./push_swap $ARG | ./checker $ARG | xargs echo -n
+TMP=`./push_swap $ARG | wc -l`
 if [ $TMP -gt $MAX500 ] ; then
     MAX500=$TMP
     echo $ARG > $FILE2
@@ -39,6 +39,6 @@ echo
 echo "cat $FILE1"
 echo "cat $FILE2"
 echo
-echo "./push_swap_src/push_swap \`cat $FILE1\` | wc -l"
-echo "./push_swap_src/push_swap \`cat $FILE2\` | wc -l"
+echo "./push_swap \`cat $FILE1\` | wc -l"
+echo "./push_swap \`cat $FILE2\` | wc -l"
 echo
