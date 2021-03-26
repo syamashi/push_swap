95da8d6ce067aa43023f143ffff70565d1f4e6fa
syamashi <syamashi@student.42tokyo.jp>
2021-03-27 08:43:05 +0900

    \n

--- util/util_is.c
+++ util/util_is.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 21:05:34 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 08:37:37 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 08:43:01 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ bool	is_sorted(t_dlst *a)
 {
 	t_dlst	*dlst;
 
-	dlst = a->next;
-	while (dlst->value != -1)
+	dlst = a;
+	while (dlst->next->value != -1)
 	{
 		if (dlst->value > dlst->next->value)
 			return (false);
