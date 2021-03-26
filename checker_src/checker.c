fdbb84df66a75bd50b4bce2a3842e31cdbc4cf65
syamashi <syamashi@student.42tokyo.jp>
2021-03-27 08:35:42 +0900

    test

--- checker_src/checker.c
+++ checker_src/checker.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 12:06:33 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 08:16:59 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 08:35:31 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,14 +72,11 @@ void	checker(int argc, char **argv)
 		debug(a, b, ps);
 	while ((ret = get_next_line(0, &line)) >= 0)
 	{
-		if (*line)
-		{
-			change_dlst(a, b, get_command(line), true);
-			ps->ans_turn++;
-			if (ps->vflag)
-				debug(a, b, ps);
-		}
-			free(line);
+		change_dlst(a, b, get_command(line), true);
+		ps->ans_turn++;
+		if (ps->vflag)
+			debug(a, b, ps);
+		free(line);
 		if (!ret)
 			break ;
 	}
