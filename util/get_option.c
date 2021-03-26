653c9f47d159ef78a9dc4dc84d8757a092852115
syamashi <syamashi@student.42tokyo.jp>
2021-03-26 18:47:11 +0900

    test

--- util/get_option.c
+++ util/get_option.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/26 18:25:28 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/26 18:45:43 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/26 18:47:05 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	get_option(long *argc, char **argv, t_ps *ps)
 		i = 0;
 		while ((*strs)[++i])
 		{
-			printf("%c", (*strs)[i]);
 			if ((*strs)[i] == 'v')
 				ps->vflag = true;
 			else if ((*strs)[i] == 'c')
@@ -33,8 +32,8 @@ void	get_option(long *argc, char **argv, t_ps *ps)
 			else
 				exit(ps_error());
 		}
-		strs++;
-		argv++;
+		(*strs)++;
+		(*argv)++;
 		(*argc)--;
 	}
 }
