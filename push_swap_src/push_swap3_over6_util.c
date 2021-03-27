759314f8fcd516ca7959028938d2103fded455a9
syamashi <syamashi@student.42tokyo.jp>
2021-03-27 15:54:23 +0900

    tfin

--- push_swap_src/push_swap3_over6_util.c
+++ push_swap_src/push_swap3_over6_util.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 19:39:13 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 15:48:43 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 15:54:16 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,9 @@ void	allsort(t_dlst *a, t_dlst *b, t_ps *ps, long size)
 		t.fin = fin_b_search(a, b, ps);
 	else
 		t.fin = size + ps->awant;
+	ft_putstr_fd("t.fin:",2);
+	ft_putstr_fd(t.fin,2);
+	ft_putstr_fd("\n",2);
 	a_dfs(a, b, &t, 0);
 	ansjoin(ps, &t);
 	dlst_update_ans(a, b, &t);
