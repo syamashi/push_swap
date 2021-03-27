fc778768d00968ca692625778ce00e6b2928f9b0
syamashi <syamashi@student.42tokyo.jp>
2021-03-27 16:06:29 +0900

    aa

--- push_swap_src/push_swap3_over6.c
+++ push_swap_src/push_swap3_over6.c
@@ -6,7 +6,7 @@
 /*   By: syamashi <syamashi@student.42.tokyo>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 19:58:34 by syamashi          #+#    #+#             */
-/*   Updated: 2021/03/27 15:56:47 by syamashi         ###   ########.fr       */
+/*   Updated: 2021/03/27 16:06:23 by syamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,36 +108,28 @@ void	sort_over6(t_dlst *a, t_dlst *b, t_ps *ps)
 	half_set(a, b, ps);
 	while (ps->awant != ps->size)
 	{
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("a\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("a\n", 1);
 		b_settle_top(a, b, ps);
 		while ((size = dlst_size(b)) > SORTSIZE)
 			b_quick_sort(a, b, ps, size);
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("b\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("b\n", 1);
 		if (size)
 			allsort(a, b, ps, size);
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("c\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("c\n", 1);
 		while ((size = dlst_size(b)) > SORTSIZE)
 			b_quick_sort(a, b, ps, size);
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("d\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("d\n", 1);
 		while ((size = search_a_size(a, ps)) && size <= SORTSIZE)
 			allsort(a, b, ps, size);
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("e\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("e\n", 1);
 		if (size)
 			a_quick_sort(a, b, ps, size);
-		if (ps->awant == 96)
-		{
-			debug(a, b, ps);
-			ft_putstr_fd("\n size:", 2);
-			ft_putnbr_fd(size, 2);
-			ft_putstr_fd("\n", 2);
-			break;
-		}
-//		ft_putnbr_fd(ps->awant, 1);
-//		ft_putstr_fd("f\n", 1);
+		ft_putnbr_fd(ps->awant, 1);
+		ft_putstr_fd("f\n", 1);
 	}
 }
